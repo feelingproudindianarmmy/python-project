8211d3ed9de5d2506c2a2e359b4e54f3368fadd8
feelingproudindianarmy <celestialh4@gmail.com>
2024-09-14 20:16:36 +0545

    my third commit

--- mathematical magic/prime number/prime.py
+++ mathematical magic/prime number/prime.py
@@ -4,7 +4,7 @@ num=int(input("enter your number"))
 
 for i in range(2,int(sqrt(num))+1):
     if (num%i)==0:
-         print("the number is not prime")
-         break
-else:
-    print("the numberr is prime")
+        print("the number is not prime")
+        break
+    else:
+        print("the numberr is prime")
