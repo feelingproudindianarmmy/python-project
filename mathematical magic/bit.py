f0bd32ecf80dfe79cf0f21a740f3c086e688550a
feelingproudindianarmy <celestialh4@gmail.com>
2024-09-19 18:04:57 +0545

    bindas

--- mathematical magic/bit.py
+++ mathematical magic/bit.py
@@ -1,4 +1,13 @@
-num1=11
+num1=69
+num2=52
+print(bin(num2))
+print(bin(num1))
+print(num1&num2)
+print(num1 | num2)
+print(~num2)
+print(num1^num2)
+#next program
+num1=20
 if(num1^1==num1+1):
     print("number is a even number")
 
